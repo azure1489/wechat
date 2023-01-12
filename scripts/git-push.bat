6b285488f36101817b812d91cc2f55881c77e4da
azure <304946629@qq.com>
2023-01-13 02:06:03 +0800

    修改

---
+++ scripts/git-push.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001
+
+
+echo "------------提交------------------------------"
+
+git add .
+
+git commit -m "修改"
+
+git push origin master
+
---
+++ scripts/git-tag.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001
+
+set tagV=v0.0.3
+
+echo "------------%tagV%------------------------------"
+
+git tag -a %tagV% -m "%tagV%"
+
+git push origin %tagV%
+
