96658dff24c44ed3116ea888dc79af4e765c0131
azure1489 <azure1489@gmail.com>
2023-11-29 02:58:31 +0800

    修改

--- callbackevent/message/voice.go
+++ callbackevent/message/voice.go
@@ -2,8 +2,8 @@ package message
 
 // Voice 语音消息
 type Voice struct {
-	VoiceLen string `json:"voice_len"`
-	VoiceHex string `json:"voice_hex"`
+	VoiceLen  string `json:"voice_len"`
+	VoiceData string `json:"voice_data"`
 }
 
 // GroupVoice 语音消息
