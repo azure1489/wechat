3c5b1ff6c766f033b3a9514a19a1589c5b81d88c
azure <304946629@qq.com>
2024-04-25 16:40:49 +0800

    修改

--- callbackevent/message/gif.go
+++ callbackevent/message/gif.go
@@ -3,6 +3,7 @@ package message
 // Gif gif消息
 type Gif struct {
 	GifPath string `json:"gif_path"`
+	Desc    string `json:"desc"`
 }
 
 // GroupGif gif消息
@@ -10,3 +11,46 @@ type Gif struct {
 // 	Gif
 // 	CommonGroupMsg
 // }
+
+type EmojiMsg struct {
+	Emoji Emoji `xml:"emoji"`
+}
+
+type Emoji struct {
+	FromUserName      string  `xml:"fromusername,attr"`
+	ToUserName        string  `xml:"tousername,attr"`
+	Type              string  `xml:"type,attr"`
+	IDBuffer          string  `xml:"idbuffer,attr"`
+	MD5               string  `xml:"md5,attr"`
+	Len               string  `xml:"len,attr"`
+	ProductID         string  `xml:"productid,attr"`
+	AndroidMD5        string  `xml:"androidmd5,attr"`
+	AndroidLen        string  `xml:"androidlen,attr"`
+	S60v3MD5          string  `xml:"s60v3md5,attr"`
+	S60v3Len          string  `xml:"s60v3len,attr"`
+	S60v5MD5          string  `xml:"s60v5md5,attr"`
+	S60v5Len          string  `xml:"s60v5len,attr"`
+	CdnURL            string  `xml:"cdnurl,attr"`
+	DesignerID        string  `xml:"designerid,attr"`
+	ThumbURL          string  `xml:"thumburl,attr"`
+	EncryptURL        string  `xml:"encrypturl,attr"`
+	AesKey            string  `xml:"aeskey,attr"`
+	ExternURL         string  `xml:"externurl,attr"`
+	ExternMD5         string  `xml:"externmd5,attr"`
+	Width             string  `xml:"width,attr"`
+	Height            string  `xml:"height,attr"`
+	TPURL             string  `xml:"tpurl,attr"`
+	TPAuthKey         string  `xml:"tpauthkey,attr"`
+	AttachedText      string  `xml:"attachedtext,attr"`
+	AttachedTextColor string  `xml:"attachedtextcolor,attr"`
+	LensID            string  `xml:"lensid,attr"`
+	EmojiAttr         string  `xml:"emojiattr,attr"`
+	LinkID            string  `xml:"linkid,attr"`
+	Desc              string  `xml:"desc,attr"`
+	GameExt           GameExt `xml:"gameext"`
+}
+
+type GameExt struct {
+	Type    string `xml:"type,attr"`
+	Content string `xml:"content,attr"`
+}
--- callbackevent/server/server.go
+++ callbackevent/server/server.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/xml"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -288,9 +290,27 @@ func (srv *Server) handleRequest() error {
 			}
 		case message.MsgTypeGif:
 			// 自定义表情消息 "msgtype":"47",
-			wcMsgItem.MsgItem = message.Gif{
+			gifMsg := message.Gif{
 				GifPath: msgInfo.Get("gif_path").String(),
 			}
+
+			if emojiMsg, err := getEmojiMsg(msgContent); err == nil {
+				base64String := emojiMsg.Emoji.Desc
+				if base64String != "" {
+					if desc, err := decodeString(base64String); err == nil {
+						desc = strings.TrimSpace(desc)
+						desc = strings.ReplaceAll(desc, "\n", "")
+
+						if desc2, err := getDesc(desc); err == nil {
+							gifMsg.Desc = desc2
+						}
+
+					}
+				}
+			}
+
+			wcMsgItem.MsgItem = gifMsg
+
 			// fromtype: 1=个人消息,2=群消息
 			if fromType == "1" {
 				wcMsgItem.EventType = message.PCRecvGifImgMsgEvent
@@ -882,4 +902,66 @@ func (srv *Server) handleRequest() error {
 	// }
 
 	return srv.messageHandler(msgItemList)
+}
+
+// 过去Emoji消息
+func getEmojiMsg(emojiXmlStr string) (*message.EmojiMsg, error) {
+
+	var msg message.EmojiMsg
+	err := xml.Unmarshal([]byte(emojiXmlStr), &msg)
+	if err != nil {
+		fmt.Println("Error unmarshalling XML: ", err)
+		return nil, err
+	}
+
+	fmt.Printf("Parsed Struct: %+v\n", msg)
+
+	return &msg, nil
+}
+
+// 解码 Base64 字符串
+func decodeString(base64String string) (string, error) {
+
+	// 解码 Base64 字符串
+	data, err := base64.StdEncoding.DecodeString(base64String)
+	if err != nil {
+		fmt.Println("Error decoding string: ", err.Error())
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func getDesc(desc string) (string, error) {
+
+	// 正则表达式匹配 'zh_cn' 后的任意字符，直到 'zh_tw' 之前
+	re := regexp.MustCompile(`zh_cn\s*(.*?)\s*zh_tw`)
+
+	match := re.FindStringSubmatch(desc)
+	// && len(match) > 1
+	if match != nil {
+		// 去除换行符和额外的空格
+		cleanedText := strings.ReplaceAll(match[1], "\n", "")
+		cleanedText = strings.TrimSpace(cleanedText)
+		cleanedText = compressStr(cleanedText)
+
+		// fmt.Println("Matched Text:", cleanedText)
+
+		return cleanedText, nil
+
+	} else {
+		fmt.Println("No match found")
+
+		return "", nil
+	}
+}
+
+// 利用正则表达式压缩字符串，去除空格或制表符
+func compressStr(str string) string {
+	if str == "" {
+		return ""
+	}
+	//匹配一个或多个空白符的正则表达式
+	reg := regexp.MustCompile(`\s+`)
+	return reg.ReplaceAllString(str, "")
 }
